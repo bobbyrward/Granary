101e8d461b50e83b847c11ecfd396924c8dd161d
Bobby Ward <bobbyrward@gmail.com>
2011-11-17 11:47:47 -0600

    Add configuration file

---
+++ config.py
@@ -0,0 +1,16 @@
+
+
+# These are the torrents we want to download
+# List of regular expressions for item titles
+
+MATCH_TORRENTS = 
+[
+    # Example:
+    # '^The\.Golden\.Girls\..*HDTV.*',
+]
+
+# The url for the feed to check
+FEED_URL = 'http://replace.me/rss/'
+
+
+
