70a8b67be2d4d5ff2279a3340f75402faa383c3f
Bobby Ward <bobbyrward@gmail.com>
2011-12-01 21:38:06 -0600

    Remove obsoleted Feed class

--- granary/feed.py
+++ granary/feed.py
@@ -18,33 +18,3 @@ def get_rss_feed_entries(url):
     else:
         feed = feedparser.parse(rss_content.read())
         return reversed(feed['entries'])
-
-
-class Feed(object):
-    """Simple feed retrieval class using feedparser
-    """
-
-    def __init__(self, url):
-        self.url = url
-        self.parsed_feed = None
-
-        self.refresh()
-
-    def refresh(self):
-        """Refresh the rss feed entries"""
-
-        response = urllib2.urlopen(self.url)
-        self.parsed_feed = feedparser.parse(response.read())
-
-    def get_entries(self):
-        """Return the rss feed entries"""
-
-        if not self.parsed_feed:
-            return None
-
-        return self.parsed_feed['entries']
-
-
-
-
-
