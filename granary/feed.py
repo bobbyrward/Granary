f433375f2d0e574058e89817958fe560e8969f8e
Bobby Ward <bobbyrward@gmail.com>
2011-12-02 04:06:14 -0600

    Disable tracebacks in feed retrieval errors. It's was getting really spammy

--- granary/feed.py
+++ granary/feed.py
@@ -8,13 +8,13 @@ def get_rss_feed_entries(url):
     try:
         rss_content = urllib2.urlopen(url)
     except httplib.BadStatusLine, e:
-        traceback.print_exc()
+        #traceback.print_exc()
         return []
     except urllib2.URLError, e:
-        traceback.print_exc()
+        #traceback.print_exc()
         return []
     except Exception, e:
-        traceback.print_exc()
+        #traceback.print_exc()
         return []
     else:
         feed = feedparser.parse(rss_content.read())
